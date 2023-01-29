a746598185c569d39a7766faf33d051edea5c0f3
HugoBianquini <hugobianqui@hotmail.com>
2023-01-29 18:22:42 -0300

    create categories

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import { CategoryRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoryRepository();
+
+
+categoriesRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  categoriesRepository.create({ name, description })
+
+  return res.status(201).send()
+})
+
+export { categoriesRoutes }
