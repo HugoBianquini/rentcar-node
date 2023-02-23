371e12f70885e85003f46448232e9d36b677f24d
HugoBianquini <hugobianqui@hotmail.com>
2023-02-22 22:19:05 -0300

    refactoring - create categories usecases and controllers, organize project structure and make categories repository a singleton

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,11 +1,20 @@
-import { Category } from "../model/Category";
-import { ICreateCategoryDTO } from "./ICategoriesRepository";
+import { Category } from "../../model/Category";
+import { ICreateCategoryDTO } from "../ICategoriesRepository";
 
 class CategoryRepository {
   private categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoryRepository;
+
+  private constructor() {
     this.categories = []
+  }
+
+  public static getInstance(): CategoryRepository {
+    if (!CategoryRepository.INSTANCE) {
+      CategoryRepository.INSTANCE = new CategoryRepository()
+    }
+    return CategoryRepository.INSTANCE
   }
 
   create({ description, name }: ICreateCategoryDTO): void {
