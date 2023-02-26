477d1a2dce87186b5436e4cd762de56f34dcacff
HugoBianquini <hugobianqui@hotmail.com>
2023-02-26 15:11:53 -0300

    rename model folder to entities and add typeorm decorators on category entity

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,9 +1,19 @@
 import { v4 as uuidV4 } from 'uuid'
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
 
+@Entity("categories")
 class Category {
+
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
@@ -11,7 +21,6 @@ class Category {
       this.id = uuidV4()
     }
   }
-
 }
 
 export { Category }
