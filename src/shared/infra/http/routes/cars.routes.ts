a7494689ccc45b039c9b8879769ca8e0f52899e4
HugoBianquini <hugobianqui@hotmail.com>
2023-03-29 20:01:38 -0300

    list available cars and filter by name, brand or category

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express"
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController"
+import { ListAvailableCarsController } from "../../../../modules/cars/useCases/listAvailableCars/ListAvailableCarsController"
 import { checkAdmin } from "../middlewares/checkAdmin"
 import { checkAuthentication } from "../middlewares/checkAuthentication"
 
 const carsRoutes = Router()
 
 let createCarController = new CreateCarController()
+const listAvailableCarsController = new ListAvailableCarsController()
 
 carsRoutes.post("/", checkAuthentication, checkAdmin, createCarController.handle)
+
+carsRoutes.get("/available", listAvailableCarsController.handle)
 
 export { carsRoutes }
