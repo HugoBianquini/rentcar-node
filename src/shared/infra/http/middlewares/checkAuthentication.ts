477b3b75c60c95ea3dac38d1a42818cf5aab0634
HugoBianquini <hugobianqui@hotmail.com>
2023-04-09 13:57:51 -0300

    create refresh token route and update checkAuthentication middleware to use refresh token

--- src/shared/infra/http/middlewares/checkAuthentication.ts
+++ src/shared/infra/http/middlewares/checkAuthentication.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import { AppError } from "../../../errors/AppError";
 import { UserRepository } from "../../../../modules/accounts/infra/typeorm/repositories/UserRepository";
+import { UsersTokensRepository } from "../../../../modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
+import auth from "../../../../config/auth";
 
 interface IPayload {
   sub: string
@@ -11,6 +13,8 @@ export async function checkAuthentication(req: Request, res: Response, next: Nex
 
   const authHeader = req.headers.authorization;
 
+  const usersTokensRepository = new UsersTokensRepository()
+
   if (!authHeader) {
     throw new AppError("Token missing", 401)
   }
@@ -18,17 +22,16 @@ export async function checkAuthentication(req: Request, res: Response, next: Nex
   const [, token] = authHeader.split(" ")
 
   try {
-    const { sub: user_id } = verify(token, "secret_key") as IPayload
+    const { sub: user_id } = verify(token, auth.secret_refresh_token) as IPayload
 
-    const usersRepository = new UserRepository()
-    const user = await usersRepository.findById(user_id)
+    const userToken = await usersTokensRepository.findByUserIdAndRefreshToken(user_id, token)
 
-    if (!user) {
+    if (!userToken) {
       throw new AppError("Token missing", 401)
     }
 
     req.user = {
-      id: user.id
+      id: user_id
     }
 
     next()
