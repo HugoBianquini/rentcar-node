2d67fbec12b5b543796e28e9cfe280a6e3b755aa
HugoBianquini <hugobianqui@hotmail.com>
2023-04-09 13:30:15 -0300

    refresh token use case

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -4,6 +4,10 @@ interface IUsersTokensRepository {
 
   create({ expires_date, refresh_token, user_id }: ICreateUserTokentDTO): Promise<UserTokens>;
 
+  findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens>;
+
+  deleteById(id: string): Promise<void>;
+
 }
 
 export { IUsersTokensRepository }
