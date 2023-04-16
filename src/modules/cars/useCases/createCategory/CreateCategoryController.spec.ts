b138a1c8a5868bb2d366986533958b056546f769
HugoBianquini <hugobianqui@hotmail.com>
2023-04-15 23:36:02 -0300

    fix integration tests

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -36,13 +36,13 @@ describe("Create Category Controller", () => {
         password: "admin"
       })
 
-    const { refresh_token } = responseToken.body
+    const { token } = responseToken.body
 
     const response = await request(app).post("/categories").send({
       name: "test",
       description: "test"
     }).set({
-      Authorization: `Bearer ${refresh_token}`
+      Authorization: `Bearer ${token}`
     })
 
     expect(response.status).toBe(201)
@@ -56,13 +56,13 @@ describe("Create Category Controller", () => {
         password: "admin"
       })
 
-    const { refresh_token } = responseToken.body
+    const { token } = responseToken.body
 
     const response = await request(app).post("/categories").send({
       name: "test",
       description: "test"
     }).set({
-      Authorization: `Bearer ${refresh_token}`
+      Authorization: `Bearer ${token}`
     })
 
     expect(response.status).toBe(400)
